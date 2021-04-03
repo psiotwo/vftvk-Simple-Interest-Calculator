bf8e8688c2fe9795a40ec9e181588cfe20e89376
Petr Křemen <petr.kremen@gmail.com>
2021-04-03 14:45:23 +0200

    Project solution

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    const p = document.getElementById("principal");
+    const principal = p.value;
+    if (!principal || principal < 1) {
+        alert("Enter a positive number.");
+        p.focus();
+    } else {
+        const rate = document.getElementById("rate").value;
+        const years = document.getElementById("years").value;
+        const interest = principal * years * rate / 100;
+        const year = new Date().getFullYear() + parseInt(years);
+        document.getElementById("result").innerHTML
+            = `If you deposit <span class="result-highlight">${principal}</span>, at an interest rate of <span class="result-highlight">${rate}%</span>, you will receive an amount of <span class="result-highlight">${interest}</span>, in the year <span class="result-highlight">${year}</span>.`;
+    }
 }
-        
+
+function updateRate() {
+    const rate = document.getElementById("rate").value;
+    document.getElementById("rateValue").textContent = rate + " %";
+}
